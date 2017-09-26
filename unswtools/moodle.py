70dbe4f3d27fc2ffd00f7ec7d43d9c5ea8779d05
Stuart Prescott <s.prescott@unsw.edu.au>
2017-09-26 13:27:12 +1000

    Allow fall-thru to default credentials

--- unswtools/moodle.py
+++ unswtools/moodle.py
@@ -4,7 +4,7 @@ from moodletools import moodle
 url = "https://moodle.telt.unsw.edu.au/"
 
 
-def login(credentials):
+def login(credentials=None):
     session = sso.session(credentials, sso.MOODLE)
 
     m = moodle.Moodle(url, session)
